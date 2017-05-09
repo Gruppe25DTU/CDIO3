8f5ab341a0e8e44813052f9954c5b28d8a8823aa
lasse <lassejensen213@gmail.com>
2017-05-09 16:32:04 +0200

    Why u no work

--- CDIO3website/WebContent/admin.js
+++ CDIO3website/WebContent/admin.js
@@ -7,7 +7,41 @@ $(document).ready(function(){
 	$("#ShowUsers").click(function(){
 		updateTable();
 	});
+	
+	$("#about").click(function() {
+		alert("NIGGA");
+		$("#popup").modal('show');
+		
+	});
+	$("window").click(function() {
+		if(event.target == $("popup")) {
+			$("popup").display = "none";
+		}
+	});
 
+	var modal = document.getElementById('popup');
+
+	var btn = document.getElementById("about");
+
+	var span = document.getElementsByClassName("close")[0];
+
+	/*btn.onclick = function() {
+	    modal.style.display = "block";
+	}
+*/
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+	
+	
+	
+	
 	$("#CreateUser").click(function(){
 		if($("#CU_UserInputFields").is(":hidden"))
 		{
