247a697e037f117d8fa577056f0ee0075e56de68
Yanki Muslu <yamuslu@gmail.com>
2017-05-10 17:18:29 +0200

    Now supports CRUD

--- CDIO3website/src/rest/RestService.java
+++ CDIO3website/src/rest/RestService.java
@@ -3,9 +3,12 @@ package rest;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,7 +30,6 @@ public class RestService {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	 public Response postMsg(UserDTO data) {
-        String output = "POST:Jersey say : " + data;
         try {
           dao.getAvailableIDs().stream().min(Integer::compareTo).ifPresent(data::setUserId);
           dao.saveUser(data);
@@ -35,8 +37,25 @@ public class RestService {
           return Response.status(Status.BAD_REQUEST).build();
         }
         System.out.println(data);
+        String output = "POST:Jersey say : " + data;
         return Response.status(200).entity(output).build();
     }
+	
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response putMsg(UserDTO data, @PathParam("id") int id) {
+	  try {
+	    dao.getUser(id);
+	    if (data.getUserID() == -1) {
+	      data.setUserID(id);
+	    }
+	    dao.updateUser(data, id);
+	    return Response.ok().build();
+	  } catch (DALException e) {
+	    return Response.notModified().build();
+	  }
+	}
 	
 	
 	@GET
@@ -47,5 +66,18 @@ public class RestService {
     } catch (DALException e) {
       return null;
     }
+	}
+	
+	@DELETE
+	@Path("{id}")
+	public Response putMsg(@PathParam("id") int id) {
+	  try {
+	  if (dao.deleteUser(id)) {
+	    return Response.ok().build();
+	  }
+	  } catch (DALException e) {
+	    
+	  }
+	  return Response.status(Status.NOT_FOUND).build();
 	}
 }
